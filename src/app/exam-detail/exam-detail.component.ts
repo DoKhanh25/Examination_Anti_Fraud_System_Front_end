e32b29b20cd214f8888a8098d57e63e5d86b839c
ASUS_ROG\MyPC <tk25042002@gmail.com>
2024-02-29 15:23:22 +0700

    Chức năng thêm điểm

--- src/app/exam-detail/exam-detail.component.ts
+++ src/app/exam-detail/exam-detail.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ResultModel } from '../model/result.model';
 import moment from 'moment';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-exam-detail',
@@ -15,6 +16,8 @@ export class ExamDetailComponent implements OnInit{
     examId: "",
     username: ""
   }
+
+  grade: number = 0;
   data: any;
 
   studentName: string = ""
@@ -24,7 +27,8 @@ export class ExamDetailComponent implements OnInit{
   constructor(public router: Router, 
     public fb: FormBuilder, 
     public activateRouter: ActivatedRoute, 
-    public httpClient: HttpClient) {
+    public httpClient: HttpClient,
+    public toastrService: ToastrService) {
       
     this.textAreaForm = fb.group({
       textArea: ""
@@ -47,6 +51,25 @@ export class ExamDetailComponent implements OnInit{
           textArea: res.data.examSolution
         })
   })
+  }
+  onSubmit(){
+    this.httpClient.post<ResultModel>(`http://localhost:8080/api/admin/postGrade`, {
+      examId: this.examParticipant.examId,
+      username: this.examParticipant.username,
+      grade: this.grade
+    }).subscribe(
+      (res) => {
+        if(res.errorCode === "0"){
+          this.toastrService.success("Đã cập nhật điểm thành công")
+        }
+        if(res.errorCode === "-1"){
+          this.toastrService.error("Bạn đã nhập điểm")
+        }
+    },
+      (error) => {
+        this.toastrService.error("Lỗi server")
+      }
+      )
   }
 
 }
